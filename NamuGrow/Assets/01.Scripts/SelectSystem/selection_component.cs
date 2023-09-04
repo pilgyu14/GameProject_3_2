8b2c32302972caad553e3e8f1c4347290eb913a5
pilgyu14 <pilgue0822@naver.com>
2023-09-04 15:29:53 +0900

    0904_선택 데칼 추가

--- NamuGrow/Assets/01.Scripts/SelectSystem/selection_component.cs
+++ NamuGrow/Assets/01.Scripts/SelectSystem/selection_component.cs
@@ -9,11 +9,13 @@ public class selection_component : MonoBehaviour
 {
     private Color DefaultColor;
     private Renderer renderer;
+    private GameObject selectionDecal; 
     void Start()
     {
         renderer = GetComponent<Renderer>();
         DefaultColor = renderer.material.color;
         renderer.material.color = Color.red;
+        
     }
 
     private void OnDestroy()
